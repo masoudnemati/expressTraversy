442a90f6ab85d45a4a63c8cbf7b3ceadb1ac67f4
masoudNemati <masoud.nnr@gmail.com>
2020-05-11 14:29:19 +0430

    finished this crash course

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
+const members = require("./Members");
 
 const app = express();
 
 //init middleware
 // app.use(logger);
 
+//Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 //body porser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Homepage Route
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Member App",
+    members,
+  })
+);
 
 //set static folder
 app.use(express.static(path.join(__dirname, "public")));
